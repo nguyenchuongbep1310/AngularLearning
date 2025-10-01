61d9a40e471feb2c23dc5c5f95a59c81384a01e3
nguyenchuongbep1310 <thanhchuongansi@gmail.com>
2025-10-01 09:05:54 +0700

    update

--- src/app/services/orders-api.service.ts
+++ src/app/services/orders-api.service.ts
@@ -5,21 +5,18 @@ import { Order } from '../models/order.model';
 
 @Injectable({ providedIn: 'root' })
 export class OrdersApiService {
-  private baseUrl = 'http://localhost:3000/orders'; // API endpoint
+  private baseUrl = 'http://localhost:3000/orders';
 
   constructor(private http: HttpClient) {}
 
-  /** Get all orders */
   getAll(): Observable<Order[]> {
     return this.http.get<Order[]>(this.baseUrl);
   }
 
-  /** Get order by ID */
   getById(id: number): Observable<Order> {
     return this.http.get<Order>(`${this.baseUrl}/${id}`);
   }
 
-  /** Create order */
   create(order: Order): Observable<Order> {
     return this.http.post<Order>(this.baseUrl, {
       ...order,
@@ -27,12 +24,10 @@ export class OrdersApiService {
     });
   }
 
-  /** Update order */
   update(order: Order): Observable<Order> {
     return this.http.put<Order>(`${this.baseUrl}/${order.id}`, order);
   }
 
-  /** Delete order */
   delete(id: number): Observable<void> {
     return this.http.delete<void>(`${this.baseUrl}/${id}`);
   }
